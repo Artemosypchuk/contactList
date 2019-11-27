c569cfbf3247804ca13c041223ad5c65e8b2eabc
Artemosypchuk <tyoska111@ukr.net>
2019-11-27 22:09:11 +0200

    Convert to JSX

---
+++ public/javascript.js
@@ -0,0 +1,56 @@
+const items = [
+    {name: 'Jonathan Smith1',title:'Graphics Designer',photo_src:'https://bootdey.com/img/Content/user_1.jpg'},
+    {name: 'Jonathan Smith2',title:'Graphics Designer',photo_src:'https://bootdey.com/img/Content/user_2.jpg'},
+    {name: 'Jonathan Smith3',title:'Graphics Designer',photo_src:'https://bootdey.com/img/Content/user_3.jpg'},
+    {name: 'Jonathan Smith4',title:'Graphics Designer',photo_src:'https://bootdey.com/img/Content/user_3.jpg'},
+    {name: 'Jonathan Smith5',title:'Graphics Designer',photo_src:'https://bootdey.com/img/Content/user_1.jpg'},
+    {name: 'Jonathan Smith6',title:'Graphics Designer',photo_src:'https://bootdey.com/img/Content/user_3.jpg'}
+
+]
+    // return(
+    //     <div className="row d-flex">
+    //      <div className="col-sm-6 offset-3 block">
+    //         <div className="panel">
+    //             <div className="panel-body p-t-10">
+    //                 <div className="media-main">
+    //                     <a className="pull-left" href="{foo}">
+    //                         <img className="thumb-lg img-circle bx-s" src="https://bootdey.com/img/Content/user_1.jpg" alt="" />
+    //                     </a>
+    //                     <div className="pull-right btn-group-sm">
+    //                         <a href="{foo}" className="btn btn-success tooltips" data-placement="top" data-toggle="tooltip" data-original-title="Edit">
+    //                             <i className="fa fa-pencil"></i>
+    //                         </a>
+    //                         <a href="{foo}" className="btn btn-danger tooltips" data-placement="top" data-toggle="tooltip" data-original-title="Delete">
+    //                             <i className="fa fa-close"></i>
+    //                         </a>
+    //                     </div>
+    //                     <div className="info">
+    //                         <h4>Jonathan Smith</h4>
+    //                         <p className="text-muted">Graphics Designer</p>
+    //                     </div>
+    //                 </div>
+    //                 <div className="clearfix"></div>
+    //                 <hr />
+    //                 <ul className="social-links list-inline p-b-10  d-flex">
+    //                     <li>
+    //                         <a title="" data-placement="top" data-toggle="tooltip" className="tooltips" href="{foo}" data-original-title="Facebook"><i className="fa fa-facebook"></i></a>
+    //                     </li>
+    //                     <li>
+    //                         <a title="" data-placement="top" data-toggle="tooltip" className="tooltips" href="{foo}" data-original-title="Twitter"><i className="fa fa-twitter"></i></a>
+    //                     </li>
+    //                     <li>
+    //                         <a title="" data-placement="top" data-toggle="tooltip" className="tooltips" href="{foo}" data-original-title="LinkedIn"><i className="fa fa-linkedin"></i></a>
+    //                     </li>
+    //                     <li>
+    //                         <a title="" data-placement="top" data-toggle="tooltip" className="tooltips" href="{foo}" data-original-title="Skype"><i className="fa fa-skype"></i></a>
+    //                     </li>
+    //                     <li>
+    //                         <a title="" data-placement="top" data-toggle="tooltip" className="tooltips" href="{foo}" data-original-title="Message"><i className="fa fa-envelope-o"></i></a>
+    //                     </li>
+    //                 </ul>
+    //             </div>
+    //         </div>
+    //     </div>
+    //     </div>
+
+    // )
--- src/components/contact_list/contactItem/contact_Item.js
+++ src/components/contact_list/contactItem/contact_Item.js
@@ -1,53 +1,195 @@
 import React from "react";
-import "./contact_Item.css"
+import "./contact_Item.css";
+const e = React.createElement;
+const ContactItem = () => {
 
-const ContactItem = () =>{
-    return(
-        <div className="row">
-        <div className="col-sm-6">
-            <div className="panel">
-                <div className="panel-body p-t-10">
-                    <div className="media-main">
-                        <a className="pull-left" href="{foo}">
-                            <img className="thumb-lg img-circle bx-s" src="https://bootdey.com/img/Content/user_1.jpg" alt="" />
-                        </a>
-                        <div className="pull-right btn-group-sm">
-                            <a href="{foo}" className="btn btn-success tooltips" data-placement="top" data-toggle="tooltip" data-original-title="Edit">
-                                <i className="fa fa-pencil"></i>
-                            </a>
-                            <a href="{foo}" className="btn btn-danger tooltips" data-placement="top" data-toggle="tooltip" data-original-title="Delete">
-                                <i className="fa fa-close"></i>
-                            </a>
-                        </div>
-                        <div className="info">
-                            <h4>Jonathan Smith</h4>
-                            <p className="text-muted">Graphics Designer</p>
-                        </div>
-                    </div>
-                    <div className="clearfix"></div>
-                    <hr />
-                    <ul className="social-links list-inline p-b-10">
-                        <li>
-                            <a title="" data-placement="top" data-toggle="tooltip" className="tooltips" href="{foo}" data-original-title="Facebook"><i className="fa fa-facebook"></i></a>
-                        </li>
-                        <li>
-                            <a title="" data-placement="top" data-toggle="tooltip" className="tooltips" href="{foo}" data-original-title="Twitter"><i className="fa fa-twitter"></i></a>
-                        </li>
-                        <li>
-                            <a title="" data-placement="top" data-toggle="tooltip" className="tooltips" href="{foo}" data-original-title="LinkedIn"><i className="fa fa-linkedin"></i></a>
-                        </li>
-                        <li>
-                            <a title="" data-placement="top" data-toggle="tooltip" className="tooltips" href="{foo}" data-original-title="Skype"><i className="fa fa-skype"></i></a>
-                        </li>
-                        <li>
-                            <a title="" data-placement="top" data-toggle="tooltip" className="tooltips" href="{foo}" data-original-title="Message"><i className="fa fa-envelope-o"></i></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        </div>
-        
-    )
-}
+    return e(
+        "div", {
+            className: "row d-flex"
+        },
+        e(
+            "div", {
+                className: "col-sm-6 offset-3 block"
+            },
+            e(
+                "div", {
+                    className: "panel"
+                },
+                e(
+                    "div", {
+                        className: "panel-body p-t-10"
+                    },
+                    [
+                        e(
+                            "div", {
+                                key: 'a0',
+                                className: "media-main"
+                            },
+                            [
+                                e(
+                                    "a", {
+                                        key: "a1",
+                                        className: "pull-left",
+                                        href: "{foo}"
+                                    },
+                                    e("img", {
+                                        className: "thumb-lg img-circle bx-s",
+                                        src: "https://bootdey.com/img/Content/user_1.jpg",
+                                        alt: "none"
+                                    })
+                                ),
+                                e(
+                                    "div", {
+                                        key: "q2",
+                                        className: "pull-right btn-group-sm"
+                                    },
+                                    [
+                                        e(
+                                            "a", {
+                                                key: "w1",
+                                                href: "{foo}",
+                                                className: "btn btn-success tooltips",
+                                                dataplacement: "top",
+                                                datatoggle: "tooltip",
+                                                dataoriginaltitle: "Edit"
+                                            },
+                                            e("i", {
+                                                className: "fa fa-pencil"
+                                            })
+                                        ),
+                                        e(
+                                            "a", {
+                                                key: "e2",
+                                                href: "{foo}",
+                                                className: "btn btn-danger tooltips",
+                                                dataplacement: "top",
+                                                datatoggle: "tooltip",
+                                                dataoriginaltitle: "Delete"
+                                            },
+                                            e("i", {
+                                                className: "fa fa-close"
+                                            })
+                                        )
+                                    ]
+                                ),
+                                e(
+                                    "div", {
+                                        key: 'z0',
+                                        className: "info"
+                                    },
+                                    [
+                                        e(
+                                            "h4", {
+                                                key: "r2"
+                                            },
+                                            "Jonathan Smith"
+                                        ),
+                                        e(
+                                            "p", {
+                                                key: "t1",
+                                                className: "text-muted"
+                                            },
+                                            "Graphics Designer"
+                                        )
+                                    ]
+                                ),
+                                e("div", {
+                                    key: "1",
+                                    className: "clearfix"
+                                }),
+                                e("hr", {
+                                    key: "2"
+                                }),
+                                e(
+                                    "ul", {
+                                        key: "3",
+                                        className: "social-links list-inline p-b-10  d-flex"
+                                    },
+                                    [
+                                        e(
+                                            "li", {
+                                                key: "i1"
+                                            },
+                                            e(
+                                                "a", {
+                                                    dataplacement: 'top',
+                                                    className: "tooltips",
+                                                    href: "{foo}"
+                                                },
+                                                e("i", {
+                                                    className: "fa fa-facebook"
+                                                })
+                                            )
+                                        ),
+                                        e(
+                                            "li", {
+                                                key: "i2"
+                                            },
+                                            e(
+                                                "a", {
+                                                    dataplacement: 'top',
+                                                    className: "tooltips",
+                                                    href: "{foo}"
+                                                },
+                                                e("i", {
+                                                    className: "fa fa-twitter"
+                                                })
+                                            )
+                                        ),
+                                        e(
+                                            "li", {
+                                                key: "i3"
+                                            },
+                                            e(
+                                                "a", {
+                                                    dataplacement: 'top',
+                                                    className: "tooltips",
+                                                    href: "{foo}"
+                                                },
+                                                e("i", {
+                                                    className: "fa fa-linkedin"
+                                                })
+                                            )
+                                        ),
+                                        e(
+                                            "li", {
+                                                key: "i4"
+                                            },
+                                            e(
+                                                "a", {
+                                                    dataplacement: 'top',
+                                                    className: "tooltips",
+                                                    href: "{foo}"
+                                                },
+                                                e("i", {
+                                                    className: "fa fa-skype"
+                                                })
+                                            )
+                                        ),
+                                        e(
+                                            "li", {
+                                                key: "i5"
+                                            },
+                                            e(
+                                                "a", {
+                                                    dataplacement: 'top',
+                                                    className: "tooltips",
+                                                    href: "{foo}"
+                                                },
+                                                e("i", {
+                                                    className: "fa fa-envelope-o"
+                                                })
+                                            )
+                                        )
+                                    ]
+                                )
+                            ]
+                        )
+                    ]
+                )
+            )
+        )
+    );
+};
+
 export default ContactItem;
